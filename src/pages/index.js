ea7acb1bc02e949a3aee57a19edb279a0534df4c
MrWakeCZ <joseff.kral@gmail.com>
2023-07-23 15:12:59 +0200

    Fix: Broken links

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/public/pripojeni">
+            to="/public/pripojeni">
             CraftMania Wiki [ALPHA]
           </Link>
         </div>
